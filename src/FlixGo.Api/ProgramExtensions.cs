ffb91b3795cf343141ac9d2514c1243ef9895a11
Dilshodbek <khamidovdilshodbekk@gmail.com>
2023-05-26 12:17:04 +0500

    Azure-pipelines for CI/CD

---
+++ src/FlixGo.Api/ProgramExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace FlixGo.Api;
+
+public static class ProgramExtensions
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+}
